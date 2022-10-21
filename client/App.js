e05e38c6590d77a8a27920bf1b95a46c75200883
Jeffrey Liu <jeffrey.z.liu@duke.edu>
2022-10-21 14:03:35 -0400

    Added Icons

--- client/App.js
+++ client/App.js
@@ -4,6 +4,8 @@ import { HomeScreen } from './pages/Home';
 import { PastTripsScreen } from './pages/PastTrips';
 import { MemoriesScreen } from './pages/Memories';
 import { ProfileScreen } from './pages/Profile';
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -12,7 +14,27 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-home' : 'ios-home-outline';
+            } else if (route.name === 'Past Trips') {
+              iconName = focused ? 'ios-list-circle' : 'ios-list-circle-outline';
+            } else if (route.name === 'Memories') {
+              iconName = focused ? 'eye-sharp' : 'eye-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'ios-person-circle-sharp' : 'ios-person-circle-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Past Trips" component={PastTripsScreen} />
         <Tab.Screen name="Memories" component={MemoriesScreen} />
@@ -20,4 +42,4 @@ export default function App() {
       </Tab.Navigator>
     </NavigationContainer>
   );
-}
+} 

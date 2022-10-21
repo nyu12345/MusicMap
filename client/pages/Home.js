927643b14b4110d040d5e146ae66568c260ae84b
Jeffrey Liu <jeffrey.z.liu@duke.edu>
2022-10-21 15:11:54 -0400

    Added Map Functionality

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,10 +1,32 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 import * as React from 'react';
 
 export function HomeScreen() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });

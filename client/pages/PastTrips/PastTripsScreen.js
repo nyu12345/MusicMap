aa3b434667179bb8663273a8e1fca3e0fabbaf5e
Jason Shang <jasonshang2002@gmail.com>
2022-10-24 17:04:44 -0400

    improved search, tried to get map & statistics to share bottom sheet (hence the reorganization) but gave up lol

--- client/pages/PastTrips/PastTripsScreen.js
+++ client/pages/PastTrips/PastTripsScreen.js
@@ -2,14 +2,16 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import ExpoConstants from 'expo-constants'; 
 import { StatisticsScreen } from './StatisticsScreen';
 import { MapScreen } from './MapScreen'; 
+import { PastTripsList } from 'musicmap/pages/PastTrips/PastTripsList'; 
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export function PastTripsScreen() {
     return (
         <Tab.Navigator style={{ marginTop: ExpoConstants.statusBarHeight}} >
-                <Tab.Screen name="Map" component={MapScreen} />
-                <Tab.Screen name="Statistics" component={StatisticsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Statistics" component={StatisticsScreen} />
         </Tab.Navigator>
     );
 }

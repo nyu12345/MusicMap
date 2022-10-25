ebbcec278a015d8a7aa9ce2adc0b8587c1cec3b2
Nathan Huang <nathan.huang99@gmail.com>
2022-10-24 21:05:57 -0400

    added tabbing

---
+++ client/pages/ProfileView/Profile.js
@@ -0,0 +1,46 @@
+import { Text, View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useCallback, useState, useMemo, useRef } from 'react';
+import {Stack, createStackNavigator} from '@react-navigation/stack'
+import {LoginScreen} from './Login'
+import { MemoriesScreen } from '../Memories';
+import ExpoConstants from 'expo-constants'; 
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+export function ProfileScreen() {
+    const [loginStatus, setLogin] = useState(false); 
+    const Stack = createStackNavigator();
+
+    const handleCallback = (loginData) =>{
+      setLogin(loginData);
+    }
+
+    return (
+      <Tab.Navigator style={{ marginTop: ExpoConstants.statusBarHeight}} >
+                <Tab.Screen name="Login" component={LoginScreen} />
+                <Tab.Screen name="Memories" component={MemoriesScreen} />
+        </Tab.Navigator>
+    );
+  }
+
+  const styles = StyleSheet.create({
+  label: {
+    color: 'white',
+    margin: 20,
+    marginLeft: 0,
+  },
+  button: {
+    marginTop: 40,
+    color: 'white',
+    height: 40,
+    backgroundColor: '#ec5990',
+    borderRadius: 4,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 8,
+    backgroundColor: '#0e101c',
+  },
+});

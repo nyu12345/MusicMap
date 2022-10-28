82d51e44656bb2377d239793120f0b9985518a4d
rld39 <rld39@duke.edu>
2022-10-28 14:42:52 -0400

    location works!

--- client/pages/Home/HomeMap.js
+++ client/pages/Home/HomeMap.js
@@ -7,7 +7,7 @@ import * as Location from "expo-location";
 
 export function HomeMap() {
   const [currentLocation, setCurrentLocation] = useState(null);
-  var permission;
+  const [permissionStatus, setStatus] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -18,29 +18,35 @@ export function HomeMap() {
       } else {
         console.log("Access granted!!");
         setStatus(permission);
+        let location = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.Highest,
+          timeInterval: 200,
+          distanceInterval: 0,
+        });
+        setCurrentLocation(location);
       }
     })();
   }, []);
 
-  const getLocation = () => {
-    console.log("CLICKED");
-    console.log(permission);
-    if (permission.status === "granted") {
-      console.log("I HAVE PERMISSION");
-      let location = Location.getCurrentPositionAsync(
-        {
-          accuracy: Location.Accuracy.High,
-          timeInterval: 200,
-          distanceInterval: 0,
-        },
-        false,
-        (location) => {
-          setCurrentLocation(location);
-          console.log("update location!", location.coords);
-        }
-      )
-    }
-  };
+  // const getLocation = async () => {
+  //   console.log("CLICKED");
+  //   console.log(permissionStatus);
+  //   if (permissionStatus.status === "granted") {
+  //     console.log("I HAVE PERMISSION");
+  //     let location = await Location.getCurrentPositionAsync(
+  //       {
+  //         accuracy: Location.Accuracy.Highest,
+  //         timeInterval: 200,
+  //         distanceInterval: 0,
+  //       },
+  //       true,
+  //       (location) => {
+  //         setCurrentLocation(location);
+  //         console.log("update location!", location.coords);
+  //       }
+  //     );
+  //   }
+  // };
 
   const location = {
     latitude: 37.78825,
@@ -49,15 +55,23 @@ export function HomeMap() {
     longitudeDelta: 0.0421,
   };
 
+  let text = currentLocation
+    ? `coords: ${currentLocation.coords.latitude}, ${currentLocation.coords.longitude}`
+    : "waiting...";
+
   return (
-    // <Pressable
-    //     style={[styles.cancelButton]}
-    //     onPress={getLocation}
-    //   ></Pressable>
-    <MapView
-      style={styles.map}
-      initialRegion={location}
-      showsUserLocation={true}
-    ></MapView>
+    <>
+      {/* <Pressable
+        style={[styles.cancelButton]}
+        onPress={getLocation}
+      ></Pressable> */}
+      <Text style={styles.modalText}>{text}</Text>
+    </>
+
+    // <MapView
+    //   style={styles.map}
+    //   initialRegion={location}
+    //   showsUserLocation={true}
+    // ></MapView>
   );
 }

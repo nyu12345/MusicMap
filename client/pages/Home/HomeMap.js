280336730ef86e1602c57db52cae975bfd860422
rld39 <rld39@duke.edu>
2022-10-28 10:19:39 -0400

    working on getting user location

--- client/pages/Home/HomeMap.js
+++ client/pages/Home/HomeMap.js
@@ -1,8 +1,49 @@
 import MapView from "react-native-maps";
 import styles from "./HomeStyles";
-import Geolocation from "react-native-geolocation-service";
+import React, { useState, useEffect } from "react";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+import * as TaskManager from "expo-task-manager";
+import * as Location from "expo-location";
 
 export function HomeMap() {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  var permission;
+
+  useEffect(() => {
+    (async () => {
+      permission = await Location.requestForegroundPermissionsAsync();
+      if (!permission.granted) {
+        setStatus("Permission to access location was denied");
+        return;
+      } else {
+        console.log("Access granted!!");
+        setStatus(permission);
+      }
+    })();
+  }, []);
+
+  const getLocation = async () => {
+    console.log("CLICKED");
+    console.log(permission);
+    if (permission.status === "granted") {
+      console.log("I HAVE PERMISSION");
+      let location = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 1,
+          distanceInterval: 1,
+        },
+        false,
+        (location) => {
+          setCurrentLocation(location);
+          console.log("update location!", location.coords);
+        }
+      ).then((response) => {
+        console.log(`response: ${response}`);
+      });
+    }
+  };
+
   const location = {
     latitude: 37.78825,
     longitude: -122.4324,
@@ -11,10 +52,29 @@ export function HomeMap() {
   };
 
   return (
+    // <>
+    //   {/* <Pressable
+    //     style={[styles.cancelButton]}
+    //     onPress={watch_location}
+    //   ></Pressable> */}
     <MapView
       style={styles.map}
       initialRegion={location}
       showsUserLocation={true}
     ></MapView>
+    // </>
+    // <View style={styles.container}>
+    //   <TouchableOpacity onPress={() => getLocation} style={styles.button}>
+    //     <Text style={styles.buttonLabel}>Get Location</Text>
+    //   </TouchableOpacity>
+
+    //   {currentLocation && currentLocation.latitude && (
+    //     <Text>{currentLocation.latitude}</Text>
+    //   )}
+
+    //   {currentLocation && currentLocation.longitude && (
+    //     <Text>{currentLocation.longitude}</Text>
+    //   )}
+    // </View>
   );
 }

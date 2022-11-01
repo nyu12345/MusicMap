c4695138e5128539d42a04c9c7ff279aaac27300
Jeffrey Liu <jeffrey.z.liu@duke.edu>
2022-11-01 02:18:01 -0400

    Added a pie chart

--- client/pages/PastTrips/StatisticsScreen.js
+++ client/pages/PastTrips/StatisticsScreen.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Text, View, StyleSheet, ScrollView, RefreshControl, FlatList, TextInput } from 'react-native';
+import { Dimensions,Text, View, StyleSheet, ScrollView, RefreshControl, FlatList, TextInput } from 'react-native';
 import axios from 'axios';
 import { REACT_APP_BASE_URL } from '@env';
+import { MyPieChart } from './graphs/MyPieChart';
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
 
 export function StatisticsScreen() {
     const [statistics, setStatistics] = useState([]);
@@ -18,6 +27,44 @@ export function StatisticsScreen() {
             setStatistics(response.data);
         });
     }
+    const data = [
+        {
+            name: "Seoul",
+            population: 21500000,
+            color: "rgba(131, 167, 234, 1)",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        },
+        {
+            name: "Toronto",
+            population: 2800000,
+            color: "#F00",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        },
+        {
+            name: "Beijing",
+            population: 527612,
+            color: "red",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        },
+        {
+            name: "New York",
+            population: 8538000,
+            color: "#ffffff",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        },
+        {
+            name: "Moscow",
+            population: 11920000,
+            color: "rgb(0, 0, 255)",
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        }
+    ];
+
     return (
         <ScrollView
             contentContainerStyle={styles.scrollView}
@@ -44,6 +91,7 @@ export function StatisticsScreen() {
                         )
                 }
             </View>
+            <MyPieChart/>
         </ScrollView>
     );
 
@@ -56,4 +104,20 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
     },
+    chartConfig: {
+        backgroundColor: "#e26a00",
+        backgroundGradientFrom: "#fb8c00",
+        backgroundGradientTo: "#ffa726",
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+            borderRadius: 16
+        },
+        propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+        }
+    }
 });
---
+++ client/pages/PastTrips/graphs/MyPieChart.js
@@ -0,0 +1,136 @@
+import React, { useCallback, useState, useMemo, useRef } from 'react';
+import { Dimensions, Text, View, StyleSheet } from 'react-native';
+import { REACT_APP_BASE_URL } from '@env';
+import axios from 'axios';
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
+
+export function MyPieChart() {
+    const base_url = `${REACT_APP_BASE_URL}/users/`;
+    const [roadtrips, setRoadtrips] = useState([]);
+    const randColor = () =>  {
+        return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+    }
+
+    // get roadtrip data from API
+    if (roadtrips.length == 0) {
+        axios.get(`${REACT_APP_BASE_URL}/roadtrips/`).then((response) => {
+            setRoadtrips(response.data);
+        });
+    } else {
+        // console.log('printing');
+    }
+    let datab = [];
+    let counts = {};
+    for (const trip of roadtrips) {
+        counts[trip.startLocation] = counts[trip.startLocation] ? counts[trip.startLocation] + 1 : 1
+    }
+    for (const city of Object.keys(counts)) {
+        datab.push({
+            name: city,
+            num: counts[city],
+            color: randColor(),
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        });
+    }
+
+
+
+
+
+    return (
+        <View>
+            <Text>Start Locations</Text>
+            <PieChart
+                data={datab}
+                width={Dimensions.get("window").width}
+                height={200}
+                chartConfig={styles.chartConfig}
+                accessor={"num"}
+                backgroundColor={"transparent"}
+            />
+        </View>
+        // <View>
+        //     <Text>Bezier Line Chart</Text>
+        //     <LineChart
+        //         data={{
+        //             labels: ["January", "February", "March", "April", "May", "June"],
+        //             datasets: [
+        //                 {
+        //                     data: [
+        //                         Math.random() * 100,
+        //                         Math.random() * 100,
+        //                         Math.random() * 100,
+        //                         Math.random() * 100,
+        //                         Math.random() * 100,
+        //                         Math.random() * 100
+        //                     ]
+        //                 }
+        //             ]
+        //         }}
+        //         width={Dimensions.get("window").width} // from react-native
+        //         height={220}
+        //         yAxisLabel="$"
+        //         yAxisSuffix="k"
+        //         yAxisInterval={1} // optional, defaults to 1
+        //         chartConfig={{
+        //             backgroundColor: "#e26a00",
+        //             backgroundGradientFrom: "#fb8c00",
+        //             backgroundGradientTo: "#ffa726",
+        //             decimalPlaces: 2, // optional, defaults to 2dp
+        //             color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        //             labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        //             style: {
+        //                 borderRadius: 16
+        //             },
+        //             propsForDots: {
+        //                 r: "6",
+        //                 strokeWidth: "2",
+        //                 stroke: "#ffa726"
+        //             }
+        //         }}
+        //         bezier
+        //         style={{
+        //             marginVertical: 8,
+        //             borderRadius: 16
+        //         }}
+        //     />
+        // </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    textInput: {
+        alignSelf: "stretch",
+        marginHorizontal: 12,
+        marginBottom: 12,
+        padding: 12,
+        borderRadius: 12,
+        backgroundColor: "rgba(151, 151, 151, 0.25)",
+        color: "black",
+        textAlign: "left",
+    },
+    chartConfig: {
+        backgroundColor: "#e26a00",
+        backgroundGradientFrom: "#fb8c00",
+        backgroundGradientTo: "#ffa726",
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+            borderRadius: 16
+        },
+        propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+        }
+    }
+})

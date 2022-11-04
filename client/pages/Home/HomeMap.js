eb0afe3a3f0ccddab76f1845fae01770ac96182c
rld39 <rld39@duke.edu>
2022-11-03 20:02:27 -0400

    working on updating location

--- client/pages/Home/HomeMap.js
+++ client/pages/Home/HomeMap.js
@@ -13,7 +13,9 @@ export function HomeMap() {
   const LATITUDE_DELTA = 0.0922;
   const LONGITUDE_DELTA = 0.0421;
 
-  // requests permission if needed and sets up initial location
+  /**
+   * requests permission if needed and sets up initial location
+   */
   useEffect(() => {
     (async () => {
       permission = await Location.requestForegroundPermissionsAsync();
@@ -21,20 +23,26 @@ export function HomeMap() {
         setStatus("Permission to access location was denied");
         return;
       } else {
-        console.log("Access granted!!");
         setStatus(permission);
-        let initialLocation = await Location.getCurrentPositionAsync({
+        let location = await Location.getCurrentPositionAsync({
           accuracy: Location.Accuracy.Highest,
-          timeInterval: 200,
+          timeInterval: 10000,
           distanceInterval: 0,
         });
-        updateLocation(initialLocation);
+        updateLocation(location);
       }
     })();
-  }, []);
+  });
 
+  /**
+   * update's the current location of the user
+   * @param {Location.LocationObject} newLocation the new location
+   */
   const updateLocation = (newLocation) => {
     if (newLocation) {
+      console.log(
+        `dX: ${newLocation.coords.latitude - currentLocation.coords.latitude}, dY: ${newLocation.coords.longitude - currentLocation.coords.longitude}`
+        );
       setCurrentLocation({
         latitude: newLocation.coords.latitude,
         longitude: newLocation.coords.longitude,
@@ -45,8 +53,8 @@ export function HomeMap() {
   };
 
   let text = currentLocation
-    ? `coords: ${currentLocation.latitude}, ${currentLocation.longitude}`
-    : "waiting...";
+    ? `coords: (${currentLocation.latitude}, ${currentLocation.longitude})`
+    : "Please wait while we retrieve your location...";
 
   return (
     <>

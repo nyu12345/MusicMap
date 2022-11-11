d75edd3b7d66c2045487118c51a7cab41fd8ff0d
Jason Shang <jasonshang2002@gmail.com>
2022-11-10 23:17:18 -0500

    fix merge shit

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -2,19 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
   name: { type: String },
   spotifyUsername: { type: String },
   numFriends: { type: Number },
   profilePic: { type: String },
   friends: [{ type: Schema.Types.ObjectId, ref: "User", optional: true }],
-=======
-    name: { type: String }, 
-    spotifyUsername: { type: String }, 
-    numFriends: { type: Number }, 
-    profilePic: { type: String }, 
-    friends: [{ type: Schema.Types.ObjectId, ref: 'User', optional: true}], 
->>>>>>> 283924309b7e4ea67aa62c9c8c665954dfaba92a
 });
 
 // generate createdAt & updatedAt fields

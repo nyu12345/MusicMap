1b709595d5c31083db53319d2634437308cefcb1
Jason Shang <69175401+jason-shang@users.noreply.github.com>
2022-11-10 23:29:28 -0500

    Save main, also added functionality to upload user to database if it's his/her first time logging in (#9)
    * scuffed expo-auth-session login approach
    
    * working redirect
    
    * partial login logic
    
    * trying to get access token
    
    * spotify api is trash it sometimes works sometimes doesn't documentation is trash
    
    * can successfully retrieve access token
    
    * working SecureStore for auth
    
    * working login and logout
    
    * working login transition
    
    * infinite loop upon login, attempting to get rid of auth code
    
    * added redirect to logout
    
    * refactor, add refresh token request
    
    * some login UI fixes
    
    * some small changes
    
    * merge conflict
    
    * UI hard mock for profile page
    
    * added bottom sheet modal for adding friends UI
    
    * some UI changes
    
    * add logged in user to MongoDB if it's user's first time logging in
    
    * fix merge shit
    
    Co-authored-by: Nathan Huang <nathan.huang99@gmail.com>

--- client/App.js
+++ client/App.js
@@ -1,44 +1,87 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { HomeScreen } from './pages/Home/HomeScreen';
-import { PastTripsScreen } from './pages/PastTrips/PastTripsScreen';
-import { MemoriesScreen } from './pages/Memories';
-import { ProfileScreen } from './pages/ProfileView/Profile';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect } from 'react';
+import LoginScreen from 'musicmap/pages/LoginScreen'; 
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { LoggedInScreen } from 'musicmap/pages/LoggedInScreen'; 
+import { createStackNavigator } from '@react-navigation/stack';
+import { save, getValueFor } from "musicmap/util/SecureStore"; 
+import { REACT_APP_BASE_URL } from "@env"; 
+import axios from "axios"; 
+//import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { getAccessTokenFromSecureStorage } from "musicmap/util/TokenRequests"; 
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [initialRoute, setInitialRoute] = useState(""); 
+  const [authCode, setAuthCode] = useState("");
+
+  // get user's username from Spotify API
+
+  // async function getUserInfo() {
+  //   const accessToken = await getAccessTokenFromSecureStorage();
+
+  //   const response = await fetch("https://api.spotify.com/v1/me", {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization: `Bearer ${accessToken}`,
+  //     },
+  //   });
+
+  //   if (response) {
+  //     const responseJson = await response.json();
+  //     console.log("poop: " + responseJson); 
+  //     return [responseJson.display_name, responseJson.id, responseJson.followers.total, responseJson.images[0].url];  
+  //   } else {
+  //     console.log("getUserInfo request returned no response");
+  //   }
+  // }
+
+  const loginToParent = () => {
+    console.log("TESTU TESTSTT");
+    setAuthCode("");
+  }
+
+  useEffect(() => {
+    async function checkTokensAndSetNavigation() {
+      try {
+        // setItemAsync still needs to be implemented in the app
+        const token = await getValueFor("ACCESS_TOKEN");
+        console.log("in App level"); 
+        console.log(token); 
+
+        if (token !== undefined) {
+          console.log('non-null token, set route to loggedin'); 
+          setInitialRoute("loggedin");
+        } else {
+          console.log('null token, set route to login'); 
+          setInitialRoute("login");
+        }
+      } catch (error) {
+        setInitialRoute("login");
+      }
+
+      // if (initialRoute === "loggedin") {
+      //   props.navigation.navigate("loggedin"); 
+      // }
+    }
+    checkTokensAndSetNavigation();  
+  }); 
+
+  console.log("initialRoute: " + initialRoute)
+
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'ios-home' : 'ios-home-outline';
-            } else if (route.name === 'Past Trips') {
-              iconName = focused ? 'ios-list-circle' : 'ios-list-circle-outline';
-            } else if (route.name === 'Memories') {
-              iconName = focused ? 'eye-sharp' : 'eye-outline';
-            } else if (route.name === 'Profile') {
-              iconName = focused ? 'ios-person-circle-sharp' : 'ios-person-circle-outline';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'black',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
-        <Tab.Screen options={{ headerShown: false }} name="Past Trips" component={PastTripsScreen} />
-        <Tab.Screen options={{ headerShown: false }} name="Memories" component={MemoriesScreen} />
-        <Tab.Screen options={{ headerShown: false }} name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName={"login"}>
+        <Stack.Screen options={{ headerShown: false }} name="login" 
+        children = {props => <LoginScreen navigation = {props.navigation} authCode = {authCode} setAuthCode = {setAuthCode}/>}
+        //component={LoginScreen} 
+        />
+        <Stack.Screen options={{ headerShown: false }} name="loggedin" 
+        //component = {LoggedInScreen}
+        children = {props => <LoggedInScreen navigation = {props.navigation} loginToParent = {loginToParent}/>}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-} 
+}
---
+++ client/pages/ProfileView/ProfileScreen.js
@@ -0,0 +1,216 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  ScrollView,
+  SafeAreaView,
+  Pressable, 
+} from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { REACT_APP_BASE_URL } from "@env";
+import axios from "axios";
+import * as SecureStore from 'expo-secure-store';
+import { Linking, Networking } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import { getAccessTokenFromSecureStorage } from "musicmap/util/TokenRequests";
+import { deleteValue } from "musicmap/util/SecureStore"; 
+import { FriendCard } from "musicmap/pages/ProfileView/FriendCard"; 
+import { AddFriendBottomSheet } from "musicmap/pages/ProfileView/AddFriendBottomSheet";
+import { FriendSectionHeader } from "./FriendSectionHeader";
+
+const ProfileScreen = (props) => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState(""); 
+  const [numFollowers, setNumFollowers] = useState(0);
+  const [profilePic, setProfilePic] = useState("");
+  const emptyProfilePic = "abc_dummy.com"; 
+
+  async function getUserInfo() {
+    const accessToken = await getAccessTokenFromSecureStorage();
+
+    const response = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (response) {
+      const responseJson = await response.json();
+      setName(responseJson.display_name);
+      setUsername(responseJson.id); 
+      setNumFollowers(responseJson.followers.total);
+      setProfilePic(responseJson.images[0].url);
+    } else {
+      console.log("getUserInfo request returned no response");
+    }
+  }
+
+  async function addUserToMongoDB(name, username, numFollowers, profilePicUrl) {
+    const user = {
+      name: name, 
+      spotifyUsername: username, 
+      numFriends: numFollowers, 
+      profilePic: profilePicUrl, 
+      friends: [], 
+    }
+    axios.post(`${REACT_APP_BASE_URL}/users`, user).then((response) => {
+      console.log("success"); 
+    }).catch((err) => {
+      console.log(err); 
+    })
+  }
+
+  async function addUserIfNew(username) {
+    await axios.get(`${REACT_APP_BASE_URL}/users/${username}`).then((response) => {
+      console.log("response: " + response.data.length); 
+      if (response.data.length === 0) {
+        //setUserExists(false); 
+        addUserToMongoDB(name, username, numFollowers, profilePic)
+      }
+    }).catch((err) => {
+      console.log(err); 
+    });
+  }
+
+  useEffect(() => {
+    (async () => {
+      await getUserInfo(); 
+      await addUserIfNew(username); 
+    })();
+  });
+
+  // remove token, show Spotify log out screen, clear cookies & navigate to login screen
+  const logOut = async () => {
+    props.loginToParent();
+    await SecureStore.deleteItemAsync("AUTH_CODE");
+    console.log("DELETED AUTH");
+    await deleteValue("ACCESS_TOKEN");
+    await deleteValue("REFRESH_TOKEN");
+    await deleteValue("EXPIRATION_TIME");
+
+    Linking.openURL("https://accounts.spotify.com/en/logout"); // look into redirects?
+    Networking.clearCookies(() => {});
+
+    props.navigation.navigate("login");
+  };
+
+  // define bottom sheet modal properties
+  const bottomSheetModalRef = useRef(null);
+
+  // dummy data
+  const friends = [
+    {
+        name: "Jeffrey Liu", 
+        numFriends: 30, 
+        spotifyUsername: "jzl", 
+        friends: [], 
+        profilePic: "https://i.scdn.co/image/ab6775700000ee85601521a5282a3797015eeed6", 
+    }, 
+    {
+        name: "Nathan Huang", 
+        numFriends: 29, 
+        spotifyUsername: "nhu", 
+        friends: [], 
+        profilePic: "https://i.scdn.co/image/ab6775700000ee85601521a5282a3797015eeed6", 
+    }, 
+  ]
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView
+        style={{flex: 1, padding: 20}}
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          style={styles.profilePic}
+          source={{
+            uri: profilePic ? profilePic : emptyProfilePic,
+          }}
+        />
+        <Text style={styles.userName}> {name} </Text>
+
+        <View style={styles.userInfoWrapper}>
+          <View style={styles.userInfoItem}>
+            <Text style={styles.userInfoTitle}>{numFollowers}</Text>
+            <Text style={styles.userInfoSubTitle}>Followers</Text>
+          </View>
+          <View style={styles.userInfoItem}>
+            <Text style={styles.userInfoTitle}>5</Text>
+            <Text style={styles.userInfoSubTitle}>Roadtrips</Text>
+          </View>
+        </View>
+
+        <FriendSectionHeader bottomSheetModalRef={bottomSheetModalRef}/>
+
+        {friends.map((item) => (
+          <FriendCard name={item.name} numFriends={item.numFriends} profilePic={item.profilePic} key={item.spotifyUsername}/>
+        ))}
+
+        <Pressable style={styles.logoutButton} onPress={logOut}>
+          <Text style={styles.logoutButtonText}>LOG OUT</Text>
+        </Pressable>
+      </ScrollView>
+      <AddFriendBottomSheet bottomSheetModalRef={bottomSheetModalRef} />
+    </SafeAreaView>
+  );
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  profilePic: {
+    height: 130,
+    width: 130,
+    borderRadius: 75,
+  },
+  userName: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  userInfoWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: 10, 
+    marginBottom: 15, 
+  },
+  userInfoItem: {
+    justifyContent: 'center',
+  },
+  userInfoTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  userInfoSubTitle: {
+    fontSize: 12,
+    color: '#666',
+    textAlign: 'center',
+  },
+  logoutButton: {
+    marginTop: 10, 
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 30,
+    elevation: 3,
+    backgroundColor: '#1DB954',
+  },
+  logoutButtonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+  },
+});

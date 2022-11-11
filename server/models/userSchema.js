26ec5f093b773fd6708a4be3b6d8190011b30811
Kathie Huang <kathie.huang@duke.edu>
2022-11-10 22:47:03 -0500

    Revert "Merge main of https://github.com/nyu12345/MusicMap"
    This reverts commit a506cd9556fef58e79c144e057d70c6f851d58fa, reversing
    changes made to 959724d4179b4daf677ed9c4c21784aa8691faa5.

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -4,8 +4,6 @@ const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
     name: { type: String }, 
     spotifyUsername: { type: String }, 
-    numFriends: { type: Number }, 
-    profilePic: { type: String }, 
     friends: [{ type: Schema.Types.ObjectId, ref: 'User', optional: true}], 
 });
 

3b423a0ec942c800b95883ad824b1967cd3ff3c5
Kathie Huang <kathie.huang@duke.edu>
2022-12-05 22:27:37 -0500

    notifications work with expo.dev/notifications

--- client/App.js
+++ client/App.js
@@ -1,18 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import LoginScreen from 'musicmap/pages/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { LoggedInScreen } from 'musicmap/pages/LoggedInScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { save, getValueFor } from "musicmap/util/SecureStore";
+import * as Notifications from 'expo-notifications';
 //import { createAppContainer } from 'react-navigation';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { getAccessTokenFromSecureStorage } from "musicmap/util/TokenRequests";
+import * as Device from 'expo-device';
+let token;
+const registerForPushNotificationsAsync = async () => {
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+  return token;
+}
 
 const Stack = createStackNavigator();
 
 export default function App() {
   const [initialRoute, setInitialRoute] = useState("");
   const [authCode, setAuthCode] = useState("");
+
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect( () => {
+    // Register for push notification
+    const token = registerForPushNotificationsAsync();
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      notificationCommonHandler(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification 
+    // (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      notificationCommonHandler(response.notification);
+      notificationNavigationHandler(response.notification.request.content);
+    });
+
+    // The listeners must be clear on app unmount
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+
+  const notificationCommonHandler = (notification) => {
+    // save the notification to reac-redux store
+    console.log('A notification has been received', notification)
+  }
+
+
+  const notificationNavigationHandler = ({ data }) => {
+    // navigate to app screen
+    console.log('A notification has been touched', data)
+  }
 
   const loginToParent = () => {
     console.log("TESTU TESTSTT");

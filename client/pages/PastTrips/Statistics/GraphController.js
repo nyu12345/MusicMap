1a46ba04e7a15e7bc1f7f1d19d568c412fea2bac
Jeffrey Liu <jeffrey.z.liu@duke.edu>
2022-12-05 23:20:15 -0500

    Remove all hard mocks

--- client/pages/PastTrips/Statistics/GraphController.js
+++ client/pages/PastTrips/Statistics/GraphController.js
@@ -23,7 +23,7 @@ import { MyProgressRing } from "./graphs/MyProgressRing";
 import { MyPieChart } from "./graphs/MyPieChart";
 import { MyContributionGraph } from "./graphs/MyContributionGraph";
 
-export function StatisticsGraphs({ tripId, myStatistics, myRoadtrips, fadeAnim }) {
+export function StatisticsGraphs({ tripId, myStatistics, myRoadtrips, fadeAnim, tripSongs, aggrData }) {
   const [search, onChangeSearch] = React.useState("");
   const [progressTime, setProgressTime] = useState(0);
   useEffect(() => {
@@ -36,20 +36,92 @@ export function StatisticsGraphs({ tripId, myStatistics, myRoadtrips, fadeAnim }
   }, []);
   // console.log(tripId);
   if (tripId == -1)
-    return AllTripsGraphs({ tripId, myStatistics, myRoadtrips, progressTime });
-  return SpecificTripGraphs({ tripId, myStatistics, myRoadtrips, progressTime });
+    return AllTripsGraphs({ tripId, myStatistics, myRoadtrips, progressTime, aggrData });
+  return SpecificTripGraphs({ tripId, myStatistics, myRoadtrips, progressTime, tripSongs });
 }
 
-function AllTripsGraphs({ tripId, myStatistics, myRoadtrips, progressTime }) {
+function AllTripsGraphs({ tripId, myStatistics, myRoadtrips, progressTime, aggrData }) {
+  let vibeScore = 0;
+  let distance = 0;
+  let numSongs = 0;
+  let numMinutes = 0;
+  let uniqueDestinations = 0;
+  try {
+    // console.log(aggrData.vibeScore);
+    vibeScore = (aggrData.vibeScore / Math.max(aggrData.numSongs, 1)).toFixed(2);
+    distance = (aggrData.distance).toFixed(2);
+    numSongs = aggrData.numSongs;
+    numMinutes = (aggrData.numSeconds / 60).toFixed(0);
+    uniqueDestinations = aggrData.endCities.size;
+  } catch (e) {
+
+  }
   return (
-  <View style={{ flex: 1, justifyContent: "left", alignItems: "left", marginBottom: 40, }}>
-    <MyProgressRing vibeValue={0.85} progressTime={progressTime} allData={true} />
-    <MyContributionGraph roadtrips={myRoadtrips} progressTime={progressTime} />
+    <View style={{ flex: 1, justifyContent: "left", alignItems: "left", marginBottom: 40, }}>
+      <MyProgressRing vibeValue={vibeScore} progressTime={progressTime} allData={true} />
+      <MyContributionGraph roadtrips={myRoadtrips} progressTime={progressTime} />
+      <View style={{ justifyContent: 'left', alignItems: 'left', flexDirection: 'row', flex: 1 }}>
+        <View style={styles.boxes}>
+          <Text style={styles.title}>Total Distance</Text>
+          <Text>
+            <Text style={styles.text}>{distance}</Text>
+            <Text style={styles.title}>
+              mi
+            </Text>
+          </Text>
+
+        </View>
+        <View style={styles.boxes}>
+          <Text style={styles.title}>Songs</Text>
+          <Text style={styles.text}>{numSongs}</Text>
+        </View>
+      </View>
+      <View style={{ justifyContent: 'left', alignItems: 'left', flexDirection: 'row', flex: 1 }}>
+        <View style={styles.boxes}>
+          <Text style={styles.title}>Song time</Text>
+          <Text>
+            <Text style={styles.text}>{numMinutes}</Text>
+            <Text style={styles.title}>
+              min
+            </Text>
+          </Text>
+        </View>
+        <View style={styles.boxes}>
+          <Text style={styles.title}>Unique Destinations</Text>
+          <Text>
+            <Text style={styles.text}>{uniqueDestinations}</Text>
+          </Text>
+        </View>
+      </View>
+      <MyPieChart roadtrips={myRoadtrips} progressTime={progressTime} />
+    </View>);
+}
+
+function SpecificTripGraphs({ tripId, myStatistics, myRoadtrips, progressTime, tripSongs }) {
+  let vibeScore = 0;
+  let distance = 0;
+  let numSongs = 0;
+  let numMinutes = 0;
+  let topSpeed = 0;
+  let fastestSong = "Null";
+  try {
+    vibeScore = (tripSongs[tripId]['vibeScore'] / Math.max(tripSongs[tripId]['numSongs'], 1)).toFixed(2);
+    distance = (tripSongs[tripId]['distance']).toFixed(2);
+    numSongs = tripSongs[tripId]['numSongs'];
+    numMinutes = (tripSongs[tripId]['numSeconds'] / 60).toFixed(0);
+    topSpeed = tripSongs[tripId]['topSpeed'].toFixed(0);
+    fastestSong = tripSongs[tripId]['fastestSong'];
+  } catch (e) {
+
+  }
+  return (<View style={{ flex: 1, justifyContent: "left", alignItems: "left", marginBottom: 40, }}>
+    <MyProgressRing vibeValue={vibeScore} progressTime={progressTime} />
+    <MyPieChart roadtrips={myRoadtrips} progressTime={progressTime} />
     <View style={{ justifyContent: 'left', alignItems: 'left', flexDirection: 'row', flex: 1 }}>
       <View style={styles.boxes}>
         <Text style={styles.title}>Total Distance</Text>
         <Text>
-          <Text style={styles.text}>857</Text>
+          <Text style={styles.text}>{distance}</Text>
           <Text style={styles.title}>
             mi
           </Text>
@@ -58,25 +130,29 @@ function AllTripsGraphs({ tripId, myStatistics, myRoadtrips, progressTime }) {
       </View>
       <View style={styles.boxes}>
         <Text style={styles.title}>Songs</Text>
-        <Text style={styles.text}>35</Text>
+        <Text style={styles.text}>{numSongs}</Text>
+      </View>
+    </View>
+    <View style={{ justifyContent: 'left', alignItems: 'left', flexDirection: 'row', flex: 1 }}>
+      <View style={styles.boxes}>
+        <Text style={styles.title}>Minutes of Listening</Text>
+        <Text>
+          <Text style={styles.text}>{numMinutes}</Text>
+          <Text style={styles.title}>
+            min
+          </Text>
+        </Text>
       </View>
     </View>
     <View style={styles.boxes}>
-        <Text style={styles.title}>Minutes of Listening</Text>
+        <Text style={styles.title}>Fastest Song (may not be accurate)</Text>
         <Text>
-          <Text style={styles.text}>325</Text>
+        <Text style={styles.text}>{fastestSong} @ {topSpeed} </Text>
+          <Text style={styles.title}>
+            mph
+          </Text>
         </Text>
       </View>
-      <MyPieChart roadtrips={myRoadtrips} progressTime={progressTime} />
-  </View>);
-}
-
-function SpecificTripGraphs({ tripId, myStatistics, myRoadtrips, progressTime }) {
-  return (<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <MyProgressRing vibeValue={0.5} progressTime={progressTime} />
-    <MyPieChart roadtrips={myRoadtrips} progressTime={progressTime} />
-    <Text>Wow based on my calculations you went {parseInt(Math.random() * 20) + 80}MPH 🤓 Drive Safe!</Text>
-    <Text>🎸 Your most popular music category was Rock 🤟Rock on 😎</Text>
   </View>);
 }
 
--- client/pages/PastTrips/Statistics/StatisticsScreen.js
+++ client/pages/PastTrips/Statistics/StatisticsScreen.js
@@ -24,18 +24,21 @@ import {
   StackedBarChart,
 } from "react-native-chart-kit";
 import { StatisticsGraphs } from "./GraphController";
-
+import dayjs from "dayjs";
 
 export function StatisticsScreen() {
   // Pull in all the data here once
   const [statistics, setStatistics] = useState([]);
   const [roadtrips, setRoadtrips] = useState([]);
+  const [tripSongs, setTripSongs] = useState({});
+  const [aggrData, setAggrData] = useState({});
   const [selected, setSelected] = useState("");
 
   // setSelected("Default");
 
   const onRefresh = React.useCallback(() => {
     setStatistics([]);
+    setRoadtrips([]);
   }, []);
 
   // get statistics data from API
@@ -61,18 +64,111 @@ export function StatisticsScreen() {
   if (roadtrips.length == 0) {
     axios.get(`${REACT_APP_BASE_URL}/roadtrips/`).then((response) => {
       setRoadtrips(response.data);
+      const rts = response.data;
+      let trip_song_map = {};
+      // console.log(rts.length);
+      // let index = 0;
+      let aggrDataCollection = {};
+      aggrDataCollection['distance'] = 0;
+      aggrDataCollection['numSongs'] = 0;
+      aggrDataCollection['numSeconds'] = 0;
+      aggrDataCollection['vibeScore'] = 0;
+      aggrDataCollection['endCities'] = new Set();
+      for (const rt of rts) {
+        // if (index >= 1)
+        //   break;
+        if (rt._id == null)
+          console.log("It is null");
+        axios.get(`${REACT_APP_BASE_URL}/songs/get-trip-songs/${rt._id}`)
+          .then((response) => {
+            // console.log("Returning data");
+            // console.log(rt._id);
+            // console.log(response.data);
+            // trip_song_map[rt._id] = [];
+            let s1 = response.data;
+            s1.sort(function (a, b) {
+              return a.datestamp - b.datestamp;
+            });
+            trip_song_map[rt._id] = {};
+            trip_song_map[rt._id]['songs'] = s1;
+            let totalDistance = 0;
+            let numSongs = 0;
+            let numSeconds = 0;
+            let lastLat = 9999999;
+            let lastLong = 9999999;
+            let tempVibeScore = 0;
+            let lastDate = "";
+
+            let highestSpeed = 0;
+            let fastestSong = "None";
+            for (const s of s1) {
+              try {
+                numSongs += 1;
+                numSeconds += s.songInfo.duration_ms / 1000;
+                let curLat = s.location.latitude;
+                let curLong = s.location.longitude;
+                tempVibeScore += calcVibeScore(s);
+                // console.log("Printing");
+                // console.log("Calculated vs: " + tempVibeScore);
+                if (lastLat != 9999999) {
+                  let currDate = dayjs(s.datestamp);
+                  let timeDifference =(currDate - lastDate)/3600000;
+
+                  let distanceSinceLast = getDistanceFromLatLonInMi(lastLat, lastLong, curLat, curLong);
+                  let mph = distanceSinceLast/timeDifference;
+                  if(mph>highestSpeed) {
+                    highestSpeed = mph;
+                    fastestSong = s.title;
+                  }
+
+                  totalDistance += distanceSinceLast;
+                }
+                lastDate = dayjs(s.datestamp);
+                lastLat = curLat;
+                lastLong = curLong;
+              } catch (e) {
+                console.log("Parsing song failed id: " + rt._id);
+              }
+            }
+            trip_song_map[rt._id]['numSongs'] = numSongs;
+            trip_song_map[rt._id]['numSeconds'] = numSeconds;
+            trip_song_map[rt._id]['distance'] = totalDistance;
+            trip_song_map[rt._id]['vibeScore'] = tempVibeScore;
+            trip_song_map[rt._id]['topSpeed'] = highestSpeed;
+            trip_song_map[rt._id]['fastestSong'] = fastestSong;
+            aggrDataCollection['distance'] += totalDistance;
+            aggrDataCollection['numSongs'] += numSongs;
+            aggrDataCollection['numSeconds'] += numSeconds;
+            aggrDataCollection['vibeScore'] += tempVibeScore;
+            aggrDataCollection['endCities'].add(rt.destination);
+            // console.log("Aggr: " + aggrDataCollection['vibeScore']);
+            // console.log("Songs: " + numSongs + ", Seconds: " + numSeconds + ", Distance: " + totalDistance);
+
+            // console.log(trip_song_map.length);
+          })
+        // console.log(trip_song_map);
+        // index+=1;
+      }
+      // console.log("Total vibescore: " + aggrDataCollection['vibeScore']);
+      setTripSongs(trip_song_map);
+      setAggrData(aggrDataCollection);
       setSelected(-1);
+
+      // console.log(trip_song_map['638d612f924a417ee1633a3b']);
+      // console.log("Printing:     ---------------------");
+      // console.log(tripSongs['638d612f924a417ee1633a3b']);
     });
+
   }
   return (
-    <View style={{flex: 1}} >
+    <View style={{ flex: 1 }} >
       <View style={styles.horizontalScroll}>
-        <ScrollView horizontal={true} style={{marginVertical: 10}}>
-        <AllTrips key={-1} name={"All Roadtrips"} isSelected={selected == -1} mySetSelected={setSelected} fadeAnim={fadeAnim}></AllTrips>
+        <ScrollView horizontal={true} style={{ marginVertical: 10 }}>
+          <AllTrips key={-1} name={"All Roadtrips"} isSelected={selected == -1} mySetSelected={setSelected} fadeAnim={fadeAnim}></AllTrips>
           {
             (
               roadtrips.map((roadtrip) =>
-                <PastTrip key={roadtrip._id} roadtrip={roadtrip} isSelected={selected == roadtrip._id} mySetSelected={setSelected} fadeAnim={fadeAnim}></PastTrip>
+                <PastTrip key={roadtrip._id} roadtrip={roadtrip} isSelected={selected == roadtrip._id} mySetSelected={setSelected} fadeAnim={fadeAnim} ></PastTrip>
               )
             )
           }
@@ -88,7 +184,7 @@ export function StatisticsScreen() {
           />
         }
       >
-        <StatisticsGraphs tripId={selected} myStatistics={statistics} myRoadtrips={roadtrips} fadeAnim={fadeAnim} ></StatisticsGraphs>
+        <StatisticsGraphs tripId={selected} myStatistics={statistics} myRoadtrips={roadtrips} fadeAnim={fadeAnim} tripSongs={tripSongs} aggrData={aggrData}></StatisticsGraphs>
       </ScrollView>
     </View>
   );
@@ -147,6 +243,46 @@ const PastTrip = ({ roadtrip, isSelected, mySetSelected, fadeAnim }) => {
     </Pressable>
   );
 };
+
+function getDistanceFromLatLonInMi(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d * 0.621371;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180)
+}
+
+function calcVibeScore(song) {
+  let score = 0;
+  try {
+    if (song.songInfo) {
+      // console.log(song.songInfo.ac)
+      score += song.songInfo.acousticness;
+      score += song.songInfo.danceability;
+      score += song.songInfo.energy;
+      score += song.songInfo.instrumentalness;
+      score += song.songInfo.liveness;
+      score += song.songInfo.speechiness;
+      score += song.songInfo.valence;
+    }
+  } catch (e) {
+
+  }
+  if(isNaN(score))
+    return 0;
+  return score/7;
+}
+
 const styles = StyleSheet.create({
   bubbleSelected: {
     backgroundColor: "#0078fe",
